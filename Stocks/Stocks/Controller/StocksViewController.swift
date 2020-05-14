894af7061a5581b8809ebfa53decabff4662903f
Tony Mackay <tony@viewmodel.net>
2020-05-14 09:22:25 +0100

    feat: Add Stocks view for watchlist with demo data

--- Stocks/Stocks/Controller/StocksViewController.swift
+++ Stocks/Stocks/Controller/StocksViewController.swift
@@ -1,58 +1,59 @@
 //
-//  WatchlistViewController.swift
+//  StocksViewController.swift
 //  Stocks
 //
-//  Created by Tony Mackay on 12/05/2020.
+//  Created by Tony Mackay on 14/05/2020.
 //  Copyright © 2020 ViewModel Software. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class WatchlistViewController: UITableViewController, NSFetchedResultsControllerDelegate {
-    
+class StocksViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+
     // MARK: Outlets
     
     // MARK: Variables
     
-    let reuseIdentifier = "watchlistTableViewCell"
-    let segueIdentifier = "toStocksSegue"
+    let reuseIdentifier = "stockTableViewCell"
     var dataController: DataController!
-    var fetchedResultsController: NSFetchedResultsController<Watchlist>!
+    var fetchedResultsController: NSFetchedResultsController<Stock>!
+    var watchlist: Watchlist!
     
     // MARK: Lifecycle
-
+    
     override func viewDidLoad() {
-        print("viewDidLoad")
         super.viewDidLoad()
         
         let image = UIImage(systemName: "plus")
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: image, style:.plain, target: self, action: #selector(addTapped))
         
         setupFetchedResultsController()
+        
+        let stock = Stock(context: dataController.viewContext)
+        stock.creationDate = Date()
+        stock.companyName = "Tesla Motors Inc"
+        stock.symbol = "TSLA"
+        stock.exchangeName = "Nasdaq"
+        stock.costBasis = 790.96
+        stock.watchlist = watchlist
+        dataController.saveContext()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear")
         super.viewWillAppear(animated)
         setupFetchedResultsController()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-        print("viewDidDisappear")
         super.viewDidDisappear(animated)
         fetchedResultsController = nil
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let watchlist = sender as? Watchlist else { return }
-        if let vc = segue.destination as? StocksViewController {
-            vc.dataController = dataController
-            vc.watchlist = watchlist
-        }
-    }
-    
     // MARK: UITableView Delegates
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Watchlist: \(watchlist.name ?? "")"
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return fetchedResultsController.sections?.count ?? 1
@@ -65,22 +66,22 @@ class WatchlistViewController: UITableViewController, NSFetchedResultsController
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
         
-        let watchlist = fetchedResultsController.object(at: indexPath)
-        cell.textLabel?.text = watchlist.name
+        let stock = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = stock.symbol
+        cell.detailTextLabel?.text = stock.companyName
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("selected row at: \(indexPath.row)")
-        showStocksForWatchlist(indexPath: indexPath)
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         switch editingStyle {
         case .delete:
-            let watchlistToDelete = fetchedResultsController.object(at: indexPath)
-            dataController.viewContext.delete(watchlistToDelete)
+            let stockToDelete = fetchedResultsController.object(at: indexPath)
+            dataController.viewContext.delete(stockToDelete)
             dataController.saveContext()
         default:
             print("not implemented")
@@ -94,50 +95,26 @@ class WatchlistViewController: UITableViewController, NSFetchedResultsController
         case .insert:
             print("insert")
             tableView.insertRows(at: [newIndexPath!], with: .fade)
-            showStocksForWatchlist(indexPath: newIndexPath!)
         case .delete:
             tableView.deleteRows(at: [indexPath!], with: .fade)
         default:
             print("insert or delete not called")
         }
     }
-     
+    
     // MARK: Actions
     
     @objc func addTapped() {
-        print("Add Watchlist Tapped")
-        let alert = UIAlertController(title: "Add Watchlist", message: "", preferredStyle: UIAlertController.Style.alert)
-        
-        var textField: UITextField?
-        alert.addTextField { (nameTextField) in
-            textField = nameTextField
-            textField?.placeholder = "Watchlist name"
-        }
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: { (UIAlertAction)in
-             print("User clicked Close button")
-        }))
-        
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (UIAlertAction)in
-             print("User click OK button")
-            
-            guard let name = textField?.text else { return }
-            
-            let watchlist = Watchlist(context: self.dataController.viewContext)
-            watchlist.creationDate = Date()
-            watchlist.name = name
-            self.dataController.saveContext()
-        }))
-        
-        present(alert, animated: true, completion: nil)
+        print("Add Stock Tapped")
     }
     
     // MARK: Methods
     
     func setupFetchedResultsController() {
         if fetchedResultsController == nil {
-            print("setupFetchedResultsController")
-            let fetchRequest: NSFetchRequest<Watchlist> = Watchlist.fetchRequest()
+            let fetchRequest: NSFetchRequest<Stock> = Stock.fetchRequest()
+            let predicate = NSPredicate(format: "watchlist == %@", watchlist)
+            fetchRequest.predicate = predicate
             let sortByDate: NSSortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
             fetchRequest.sortDescriptors = [sortByDate]
             fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
@@ -149,10 +126,5 @@ class WatchlistViewController: UITableViewController, NSFetchedResultsController
         } catch {
             fatalError("The fetch could not be performed: \(error.localizedDescription)")
         }
-    }
-    
-    func showStocksForWatchlist(indexPath: IndexPath) {
-        let watchlist = fetchedResultsController.object(at: indexPath)
-        performSegue(withIdentifier: segueIdentifier, sender: watchlist)
     }
 }
